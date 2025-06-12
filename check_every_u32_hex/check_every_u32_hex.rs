661f418e87434b9b835359b23b4472b92907c283
AngelicosPhosphoros <angelicos.phosphoros@protonmail.com>
2025-06-12 20:20:30 +0200

    Added progress report to formatting checking

--- check_every_u32_hex/check_every_u32_hex.rs
+++ check_every_u32_hex/check_every_u32_hex.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::uninlined_format_args)]
 
-use core::convert::{Into, TryInto};
 use core::fmt::Write as _;
 use core::num::NonZeroUsize;
+use core::sync::atomic::AtomicU64;
+use core::sync::atomic::Ordering::Relaxed;
 use std::thread;
 use std::time::Instant;
 
@@ -11,6 +12,11 @@ mod hex;
 
 fn main() {
     println!("Starting checking values in multi thread...");
+    if let Some(flags) = option_env!("RUSTFLAGS") {
+        if flags.contains("-Zsanitizer=address") || flags.contains("-Z sanitizer=address") {
+            println!("  Using Address Sanitizer");
+        }
+    }
     let start = Instant::now();
     visit_multi_thread();
     let elapsed = start.elapsed();
@@ -22,9 +28,11 @@ fn main() {
 
 fn visit_multi_thread() {
     let num_cores = thread::available_parallelism()
-        .unwrap_or(NonZeroUsize::new(1).unwrap())
-        .get() as u64;
+        .map(NonZeroUsize::get)
+        .unwrap_or(1) as u64;
     let chunk_size = ((u64::from(u32::MAX) + 1) / num_cores) + 1;
+    let counter = AtomicU64::new(0);
+    let counter = &counter;
     thread::scope(move |scope| {
         for i in 0..num_cores {
             let start: u64 = i * chunk_size;
@@ -34,15 +42,20 @@ fn visit_multi_thread() {
                 .try_into()
                 .unwrap();
             let start: u32 = start.try_into().unwrap();
-            scope.spawn(move || check_range(start, end));
+            println!("Thread {:2}: checking range {:X}..={:X}", i, start, end);
+            scope.spawn(move || check_range(start, end, counter));
         }
     });
 }
 
-fn check_range(start: u32, end: u32) {
+const REPORT_ADD_LIMIT: u64 = 1000_000;
+const REPORT_LOG_LIMIT: u64 = 100_000_000;
+
+fn check_range(start: u32, end: u32, counter: &AtomicU64) {
     let mut s = String::with_capacity(16);
     let mut i: u64 = start.into();
     let end: u64 = end.into();
+    let mut report_counter = 0;
     while i <= end {
         let v: u32 = i.try_into().unwrap();
 
@@ -54,5 +67,18 @@ fn check_range(start: u32, end: u32) {
         assert_eq!(s, hs, "Hex views not match for {}", v);
 
         i += 1;
+        report_counter += 1;
+        if report_counter == REPORT_ADD_LIMIT {
+            update_counter(counter, report_counter);
+            report_counter = 0;
+        }
+    }
+    update_counter(counter, report_counter);
+}
+
+fn update_counter(counter: &AtomicU64, num: u64) {
+    let old_count = counter.fetch_add(num, Relaxed);
+    if (old_count + num) / REPORT_LOG_LIMIT != old_count / REPORT_LOG_LIMIT {
+        println!("Processed {:10} nums...", old_count + num)
     }
 }
