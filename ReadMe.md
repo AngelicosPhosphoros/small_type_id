b3c62c0408a2b118718319fbdd320d2b62011544
AngelicosPhosphoros <angelicos.phosphoros@protonmail.com>
2025-06-17 20:43:25 +0200

    Wrote some documentation

---
+++ ReadMe.md
@@ -0,0 +1,76 @@
+# Small Type Id
+
+This crate provides trait `HasTypeId` with associated constant `TYPE_ID` and derive macro to implement it for your types.
+
+There are 4 guarantees:
+
+1. `TYPE_ID` is a constant.
+2. Size is 32 bit.
+3. `TypeId` cannot be zero which allows [niche optimizations][1] (`size_of::<Option<TypeId>>()` is 4 bytes).
+4. Most significant bit (_MSB_) is guaranteed to be zero:
+   * Allows users to use this bit to distinguish with some other kind of id in a union (e.g. Runtime types from some scripting engine).
+   * Does **not** allow niche optimizations on current version of Rust yet.
+
+Those guarantees would never be removed (even in semver breaking releases) so you can update dependency on this crate without validating your code that rely on them.
+
+## Comparison with [`std::any::TypeId`][4]
+
+With `std::any::TypeId` as at Rust 1.87.
+
+#### Advantages
+
+1. `TYPE_ID` is a constant.
+2. Size of `small_type_id::TypeId` is guaranteed to be 32 bits.
+3. Size of `small_type_id::TypeId` is significantly smaller(4 vs 16 bytes), allowing better performance due to less usage of CPU cache.
+4. `small_type_id::TypeId` supports niche optimization for `Option<small_type_id::TypeId>`.
+5. `small_type_id::TypeId` guarantees that MSB is zero, allowing creating 32 bit identifiers by users using `union`s:
+  * Since user types would need to set MSB to 1, resulting value is still cannot be zero, allowing niche optimization.
+
+#### Downsides
+
+1. `small_type_id::HasTypeId` needs to be derived for supported types, it doesn't work automatically.
+2. `small_type_id::HasTypeId` doesn't support generic types.
+
+## Comparison with [`typeid::ConstTypeId`][2]
+
+With [`typeid`][3] version 1.0.3
+
+#### Advantages
+
+1. Has smaller size (32 bit vs 64 bit on 64-bit targets).
+2. Has defined internal representation that can be utilized by users.
+
+#### Disadvantages
+
+1. Doesn't support every type.
+
+## How it works and how to use API.
+
+Example:
+
+```rust
+use small_type_id::HasTypeId as _;
+
+#[derive(small_type_id::HasTypeId)]
+pub struct Struct {
+    pub a: u32,
+    pub b: usize,
+}
+
+#[derive(small_type_id::HasTypeId)]
+pub enum Enum {
+    A(u32), B(String)
+}
+
+// Check that they are different:
+assert_ne!(Struct::TYPE_ID, Enum::TYPE_ID);
+// Or even in compile time:
+const { assert!(Struct::TYPE_ID.as_u32() != Enum::TYPE_ID.as_u32()); };
+```
+
+More examples and implementation explanation are available in documentation.
+
+[1]: https://doc.rust-lang.org/std/option/index.html#representation
+[2]: https://docs.rs/typeid/1.0.3/typeid/struct.ConstTypeId.html
+[3]: https://crates.io/crates/typeid
+[4]: https://doc.rust-lang.org/std/any/struct.TypeId.html
--- small_type_id/src/lib.rs
+++ small_type_id/src/lib.rs
@@ -1,3 +1,120 @@
+//! This crate provides 3 things:
+//!
+//! 1. [`TypeId`] type that is used to identify type.
+//! 2. [`HasTypeId`] trait with associated constant [`TYPE_ID`][HasTypeId::TYPE_ID].
+//! 3. [`HasTypeId`](derive.HasTypeId.html) derive macro for implementing `HasTypeId` for types.
+//!
+//! `TypeId` has several guarantees about its layout and those guarantees would be held even in major semver updates:
+//!
+//! 1. `TYPE_ID` is a constant.
+//! 2. Size is 32 bit.
+//! 3. `TypeId` cannot be zero which allows [niche optimizations][1] (`size_of::<Option<TypeId>>()` is 4 bytes).
+//! 4. Most significant bit (_MSB_) is guaranteed to be zero:
+//!    * Allows users to use this bit to distinguish with some other kind of id in a union (e.g. Runtime types from some scripting engine).
+//!
+//! Further guarantees can change with versions of this crate.
+//!
+//! `TypeId` doesn't depends on contents of the type so it would remain same if it changes.
+//!
+//! `TypeId` can change on following changes:
+//!     * `small_type_id` crate version changes
+//!     * placement of the type changes (e.g. type moved into different module)
+//!     * compiler version changes
+//!     * version of a crate, where the type is declared, changes.
+//!
+//! ### Limitations
+//!
+//! Doesn't support non-static types and types with generic parameters.
+//! If you want to derive `HasTypeId` trait on such type, consider using [newtype][2] pattern.
+//!
+//! ```
+//! # use small_type_id::HasTypeId as _;
+//! #[derive(small_type_id::HasTypeId)]
+//! struct VecWithInts(/* wrapped generic type*/ Vec<i32>);
+//! ```
+//!
+//! ### How uniqueness of TypeIds are enforced
+//!
+//! Uniqueness
+//!
+//! ## Examples
+//!
+//! Use for distinguishing 2 types.
+//!
+//! ```
+//! # use small_type_id::HasTypeId as _;
+//! #[derive(small_type_id::HasTypeId)]
+//! struct A;
+//! #[derive(small_type_id::HasTypeId)]
+//! struct B;
+//!
+//! assert_ne!(A::TYPE_ID, B::TYPE_ID);
+//! // Test in compile time:
+//! const { assert!(A::TYPE_ID.as_u32() != B::TYPE_ID.as_u32()) };
+//! ```
+//!
+//! Detect that types are same or not in generic code in compile time.
+//!
+//! ```
+//! # use small_type_id::HasTypeId;
+//! const fn is_types_unique<T0, T1, T2>()->bool
+//! where
+//!     T0: HasTypeId,
+//!     T1: HasTypeId,
+//!     T2: HasTypeId,
+//! {
+//!     let types = [T0::TYPE_ID, T1::TYPE_ID, T2::TYPE_ID];
+//!     let mut i = 0;
+//!     // Cannot use for loops because it is a const function.
+//!     while i < types.len() {
+//!         let t = types[i];
+//!         let mut j = i + 1;
+//!         while j < types.len() {
+//!             if t.as_u32() == types[j].as_u32() {
+//!                 return false;
+//!             }
+//!             j += 1;
+//!         }
+//!         i += 1;
+//!     }
+//!     true
+//! }
+//!
+//! #[derive(small_type_id::HasTypeId)]
+//! struct A;
+//! #[derive(small_type_id::HasTypeId)]
+//! struct B;
+//! #[derive(small_type_id::HasTypeId)]
+//! enum C{ A, B}
+//!
+//! const { assert!(is_types_unique::<A, B, C>()) };
+//! const { assert!(! is_types_unique::<A, B, A>()) };
+//! assert!(is_types_unique::<A, B, C>());
+//! assert!(! is_types_unique::<A, B, A>());
+//! ```
+//!
+//! ## Implementation details
+//!
+//! This implementation details are subject to change and should be not relied upon.
+//!
+//! Type id is computed by hashing string that contain full module
+//! path, type name and crate version. Usage of module path allows to distinguish between
+//! types with same name declared in different places, usage of crate version makes same type from
+//! different versions of the same crate be interpreted as different types (which is correct behaviour).
+//!
+//! Current computation algorithm is [xxhash32][3].
+//!
+//! Also, we utilize [`ctor`][4] crate to register that type in global linked list and run verification
+//! of uniqueness of `TypeId`s.
+//!
+//!
+//!
+//! [1]: https://doc.rust-lang.org/std/option/index.html#representation
+//! [2]: https://rust-unofficial.github.io/patterns/patterns/behavioural/newtype.html
+//! [3]: https://xxhash.com/
+//! [4]: https://crates.io/crates/ctor
+//!
+
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(clippy::uninlined_format_args)]
 #![cfg_attr(not(test), no_std)]
@@ -8,6 +125,47 @@ mod hex;
 mod implementation;
 
 pub use implementation::private;
+
+/// Implements [`HasTypeId`] trait and registers implementation for runtime verification.
+///
+/// Example:
+/// ```
+/// # use small_type_id::HasTypeId as _;
+/// #[derive(small_type_id::HasTypeId)]
+/// struct FirstStruct;
+///
+/// #[derive(small_type_id::HasTypeId)]
+/// struct SecondStruct;
+///
+/// assert_ne!(FirstStruct::TYPE_ID, SecondStruct::TYPE_ID);
+/// ```
+///
+/// It doesn't mix up types with same name from different modules:
+/// ```
+/// # use small_type_id::HasTypeId as _;
+/// #[derive(small_type_id::HasTypeId)]
+/// struct Struct;
+///
+/// mod inner {
+///     #[derive(small_type_id::HasTypeId)]
+///     pub struct Struct;
+/// }
+///
+/// assert_ne!(Struct::TYPE_ID, inner::Struct::TYPE_ID);
+/// ```
+///
+/// It doesn't support generic types, including lifetimes:
+///
+/// ```compile_fail
+/// #[derive(small_type_id::HasTypeId)]
+/// struct Generic<T>(T);
+/// ```
+///
+/// ```compile_fail
+/// #[derive(small_type_id::HasTypeId)]
+/// struct Generic<'a>(&'a u32);
+/// ```
+///
 pub use small_type_id_proc_macro::HasTypeId;
 
 #[derive(Clone, Copy, Eq, PartialEq, PartialOrd, Ord, Hash)]
@@ -104,3 +262,18 @@ impl core::fmt::Display for ErrorFromZeroBytes {
 }
 
 impl core::error::Error for ErrorFromZeroBytes {}
+
+#[cfg(doctest)]
+#[doc = include_str!("../../Readme.md")]
+pub struct ReadmeDoctests;
+
+/// ```compile_fail
+/// # use small_type_id::HasTypeId as _;
+/// #[derive(small_type_id::HasTypeId)]
+/// struct A;
+/// #[derive(small_type_id::HasTypeId)]
+/// struct B;
+/// const { assert!(A::TYPE_ID.as_u32() == B::TYPE_ID.as_u32()) };
+/// ```
+#[cfg(doctest)]
+pub struct ExtraDocTests;
